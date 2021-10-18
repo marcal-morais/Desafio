140b9e0684453eafb0355e4c7108db18f623c7d6
marcal-morais <marcal.2@gmail.com>
2021-10-18 00:59:27 -0300

    Funcionalidade ordenar por colunas

--- src/components/ServersTable/index.js
+++ src/components/ServersTable/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import DataContext from '../DataContext';
 import { useContext } from 'react';
 import './styles.css';
@@ -5,6 +6,12 @@ import '../../styles/global.css';
 
 function ServersTable() {
   const { data, setData, servidoresSelecionados, cpuTotal, memoriaTotal, discoTotal } = useContext(DataContext);
+  const [ordem, setOrdem] = useState({
+    hostname: 0,
+    memoria: 0,
+    cpu: 0,
+    disco: 0,
+  })
   function checkItem(item) {
     item.checked = !item.checked;
     setData(data);
@@ -12,6 +19,46 @@ function ServersTable() {
     cpuTotal();
     memoriaTotal();
     discoTotal();
+  }
+  function ordenar(campo) {
+    switch (campo) {
+      case 'hostname':
+        if (ordem.hostname) {
+          data.sort((a, b) => b.hostname > a.hostname);
+          setOrdem(state => ({ ...state, hostname: false }));
+        } else {
+          data.sort((a, b) => a.hostname > b.hostname);
+          setOrdem(state => ({ ...state, hostname: true }));
+        }
+        break;
+      case 'memoria':
+        if (ordem.memoria) {
+          data.sort((a, b) => Number(b.configuracao.memoryProvisioned) - Number(a.configuracao.memoryProvisioned));
+          setOrdem(state => ({ ...state, memoria: false }));
+        } else {
+          data.sort((a, b) => Number(a.configuracao.memoryProvisioned) - Number(b.configuracao.memoryProvisioned));
+          setOrdem(state => ({ ...state, memoria: true }));
+        }
+        break;
+      case 'cpu':
+        if (ordem.cpu) {
+          data.sort((a, b) => Number(b.configuracao.cpuProvisioned) - Number(a.configuracao.cpuProvisioned));
+          setOrdem(state => ({ ...state, cpu: false }));
+        } else {
+          data.sort((a, b) => Number(a.configuracao.cpuProvisioned) - Number(b.configuracao.cpuProvisioned));
+          setOrdem(state => ({ ...state, cpu: true }));
+        }
+        break;
+      case 'disco':
+        if (ordem.disco) {
+          data.sort((a, b) => Number(b.configuracao.totalDiskGB) - Number(a.configuracao.totalDiskGB));
+          setOrdem(state => ({ ...state, disco: false }));
+        } else {
+          data.sort((a, b) => Number(a.configuracao.totalDiskGB) - Number(b.configuracao.totalDiskGB));
+          setOrdem(state => ({ ...state, disco: true }));
+        }
+        break;
+    }
   }
   return (
     <div className="box">
@@ -24,10 +71,26 @@ function ServersTable() {
             <thead>
               <tr>
                 <th>Select</th>
-                <th>Hostname</th>
-                <th>Memória</th>
-                <th>vCPUs</th>
-                <th>Disco</th>
+                <th>
+                  <a className="pointer" onClick={() => ordenar('hostname')}>
+                    Hostname
+                  </a>
+                </th>
+                <th>
+                  <a className="pointer" onClick={() => ordenar('memoria')}>
+                    Memória
+                  </a>
+                </th>
+                <th>
+                  <a className="pointer" onClick={() => ordenar('cpu')}>
+                    vCPUs
+                  </a>
+                </th>
+                <th>
+                  <a className="pointer" onClick={() => ordenar('disco')}>
+                    Disco
+                  </a>
+                </th>
                 <th>IP</th>
               </tr>
             </thead>
